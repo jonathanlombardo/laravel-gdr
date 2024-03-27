074be4a441d243c7a7336a0fbe4954ebde74a6fe
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-27 18:30:20 +0100

    feat: added required in the form

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -9,27 +9,27 @@
 
         <div class="mb-3">
           <label for="name" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="name" name="name" placeholder="zio peppo">
+          <input type="text" class="form-control" id="name" name="name" placeholder="zio peppo" required>
         </div>
 
         <div class="mb-3">
           <label for="attack" class="form-label">Attacco</label>
-          <input type="number" class="form-control" id="attack" name="attack" placeholder="99">
+          <input type="number" class="form-control" id="attack" name="attack" placeholder="99" required>
         </div>
 
         <div class="mb-3">
           <label for="defence" class="form-label">Difesa</label>
-          <input type="number" class="form-control" id="defence" name="defence" placeholder="99">
+          <input type="number" class="form-control" id="defence" name="defence" placeholder="99" required>
         </div>
 
         <div class="mb-3">
           <label for="speed" class="form-label">Velocità</label>
-          <input type="number" class="form-control" id="speed" name="speed" placeholder="99">
+          <input type="number" class="form-control" id="speed" name="speed" placeholder="99" required>
         </div>
 
         <div class="mb-3">
           <label for="life" class="form-label">Punti Vita</label>
-          <input type="number" class="form-control" id="life" name="life" placeholder="99">
+          <input type="number" class="form-control" id="life" name="life" placeholder="99" required>
         </div>
 
         <div class="mb-3">
