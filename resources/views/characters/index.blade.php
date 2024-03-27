fa6c7ef7ca6f50a02af373498627b696441cb29d
SimoneCappadona <simocappa95@gmail.com>
2024-03-27 18:07:11 +0100

    add btn for show/edit/detail

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,36 +1,43 @@
 @extends('layouts.main')
 
-
+@section('assets')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
 @section('maincontent')
-    <h1 class="text-center mt-5">Character</h1>
+    <h1 class="text-center mt-5">Personaggio</h1>
     <div class="text-center">
-      <a href="{{route('characters.create')}}" class="btn btn-primary">Nuovo Character</a>
+        <a href="{{ route('characters.create') }}" class="btn btn-primary">Nuovo Personaggio</a>
     </div>
     <div class="container p-5">
         <table class="table">
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Attack</th>
-                    <th>Defence</th>
-                    <th>Speed</th>
-                    <th>Life</th>
+                <tr class="text-center">
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Attacco</th>
+                    <th>Difesa</th>
+                    <th>Velocità</th>
+                    <th>Punti Vita</th>
+                    <th>Dettagli Personaggio</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse ($characters as $character)
-                    <tr>
+                    <tr class="text-center">
                         <th>{{ $character->name }}</td>
                         <td>{{ $character->description }}</td>
                         <td>{{ $character->attack }}</td>
                         <td>{{ $character->defence }}</td>
                         <td>{{ $character->speed }}</td>
                         <td>{{ $character->life }}</td>
+                        <td><a href="{{ route('characters.show', $character) }}"
+                                class="btn btn-primary"><i class="fa-solid fa-eye"></i></a></td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="100%">Character Not Found</td>
+                        <td colspan="100%">Nessun Personaggio Trovato</td>
                     </tr>
                 @endforelse
             </tbody>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,12 +1,11 @@
 <header class="py-3 bg-primary text-white">
   <div class="container d-flex justify-content-between align-items-center">
 
-    <span>NewProject Header</span>
+    <span>Laravel GDR</span>
     <nav>
       <ul class="d-flex align-items-center gap-3">
-        <li><a href="{{route('items')}}">Item</a></li>
-        <li><a href="{{route('characters.index')}}">Character</a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="{{route('items')}}">Strumenti</a></li>
+        <li><a href="{{route('characters.index')}}">Personaggi</a></li>
       </ul>
     </nav>
   </div>
