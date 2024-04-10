7519ab9d85791ee48a104e6fc6bfa075cc092c02
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-04-10 16:25:20 +0200

    feat: add Type Seeder Migration & relationship with Characters

--- app/Models/Characters.php
+++ app/Models/Characters.php
@@ -21,4 +21,8 @@ class Characters extends Model
   public function get_description($n_chars = 75){
     return ($n_chars < strlen($this->description)) ? substr($this->description, 0, $n_chars ). '...' : $this->description; 
   }
+
+  public function type(){
+    return $this->belongsTo(Type::class);
+  }
 }
