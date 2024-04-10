6e2b6dc2ebd5bc39d97be56b279e3710db43f3e1
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-10 18:03:37 +0200

    fix: type imgs

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -5,31 +5,32 @@ namespace Database\Seeders;
 use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Vite;
 
 class TypeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $file = fopen(__DIR__ . "/../csv/types.csv", "r");
-        $keys = fgetcsv($file);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $file = fopen(__DIR__ . "/../csv/types.csv", "r");
+    $keys = fgetcsv($file);
 
-        
 
-        while ($type_data = fgetcsv($file)) {
-            
-            $type = new Type;
-            $type->name = $type_data[0];
-            $type->image = __DIR__ . $type_data[1];
-            $type->description = $type_data[2];
 
-            $type->save();
+    while ($type_data = fgetcsv($file)) {
 
-            
-        }
+      $type = new Type;
+      $type->name = $type_data[0];
+      $type->image = Vite::asset("resources/$type_data[1]");
+      $type->description = $type_data[2];
+
+      $type->save();
+
+
     }
+  }
 }
